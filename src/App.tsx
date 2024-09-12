3f8b16cdf9988035bdb449c370a5708504174fe0
Weeraphat2000 <weeraphat.main@gmail.com>
2024-09-13 01:01:28 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,6 @@ const sehema = yup.object().shape({
 function App() {
   const [open, setOpen] = useState(false);
   const [isConfirm, setIsConfirm] = useState<boolean>(false);
-
   const price = 1_250;
 
   const handleCloseDialog = () => {
